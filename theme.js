dd1acf4ca733e8485c4e53544d0398c3bc4ef717
Aashish384 <130991768+Aashish384@users.noreply.github.com>
2023-08-13 09:25:59 +0930

    Add files via upload

---
+++ theme.js
@@ -0,0 +1,28 @@
+const theme = {
+  fontFamily: "'Source Sans Pro', sans-serif",
+  headings: {
+    fontFamily: "'Source Sans Pro',  sans-serif",
+  },
+  colors: {
+    primary: [
+      "#EDF2FF",
+      "#DBE4FF",
+      "#BAC8FF",
+      "#91A7FF",
+      "#748FFC",
+      "#5C7CFA",
+      "#4C6EF5",
+      "#4263EB",
+      "#3B5BDB",
+      "#364FC7",
+    ],
+  },
+  primaryColor: "primary",
+  // colorScheme: "dark",
+  primaryShade: {
+    light: 6,
+    dark: 6,
+  },
+};
+
+export default theme;
